f3dea056b412bab64c5750d648002ebd0a282b3d
Saeed Darvish <sd.saeed.darvish@gmail.com>
2016-06-14 12:30:17 +0430

    Create Admin Controller With Simple Action

---
+++ app/code/community/Shareino/Sync/controllers/Adminhtml/ConfigController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: saeed
+ * Date: 6/8/16
+ * Time: 4:19 PM
+ */
+class Shareino_Sync_Adminhtml_ConfigController extends Mage_Adminhtml_Controller_Action
+{
+
+    public function indexAction()
+    {
+        echo "Shareino_Sync_Adminhtml_ConfigController_IndexAction";
+    }
+
+    public function synchronizeAction()
+    {
+        echo "Shareino_Sync_Adminhtml_ConfigController_synchronizeAction";
+
+    }
+
+}
