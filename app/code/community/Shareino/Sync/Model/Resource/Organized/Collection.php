3774399460f9b0cd43d13720a81e053d674dc7e5
Saeed Darvish <sd.saeed.darvish@gmail.com>
2016-09-04 16:03:19 +0430

    Free commit

---
+++ app/code/community/Shareino/Sync/Model/Resource/Organized/Collection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: saeed
+ * Date: 8/17/16
+ * Time: 3:23 PM
+ */
+class Shareino_Sync_Model_Organized_Collection
+    extends Mage_Core_Model_Resource_Db_Collection_Abstract{
+    protected function _constuct(){
+        $this->_init('Shareino_sync/organized');
+    }
+}
