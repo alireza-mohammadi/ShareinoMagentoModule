997a8e97a7591aabb4fc701166ac4ff8c30dfd82
Saeed Darvish <sd.saeed.darvish@gmail.com>
2016-09-22 16:15:00 +0330

    fix minor bugs

--- app/code/community/Shareino/Sync/Model/Resource/Synced.php
+++ app/code/community/Shareino/Sync/Model/Resource/Synced.php
@@ -9,6 +9,6 @@
 class Shareino_Sync_Model_Resource_Synced extends Mage_Core_Model_Resource_Db_Abstract{
     protected function _construct()
     {
-        $this->_init('sync/synced', 'id_shareino_synced');
+        $this->_init('sync/synced', 'id_shareino_sync');
     }
 }
