bb5e39bb09cb6ec8a6d22206c7e37ea92dcc8217
Saeed Darvish <sd.saeed.darvish@gmail.com>
2016-09-24 12:39:27 +0330

    Create Controller

---
+++ app/code/community/Shareino/Sync/controllers/Adminhtml/SyncedController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Saeed Darvish.
+ * Email : sd.saeed.darvish@gmail.com
+ * mobile : 09179960554
+ */
+class Shareino_Sync_Adminhtml_SyncedController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        echo "Synced Controller";
+    }
+}
