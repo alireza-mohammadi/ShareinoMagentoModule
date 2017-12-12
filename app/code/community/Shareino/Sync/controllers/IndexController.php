77323c7fac02f6d7d20280fe689f48dcd2a416e6
AliRezaMohammadiDoost <alireza.mohammadi.1990@gmail.com>
2017-12-12 10:18:51 +0330

    چک کردن موجود بودن ماژول

--- app/code/community/Shareino/Sync/controllers/IndexController.php
+++ app/code/community/Shareino/Sync/controllers/IndexController.php
@@ -5,8 +5,15 @@ class Shareino_Sync_IndexController extends Mage_Core_Controller_Front_Action
 
     public function indexAction()
     {
-        $this->loadLayout();
-        $this->renderLayout();
+        //' SELECT * FROM `core_config_data` WHERE `path` LIKE '%shareino%''
+
+        if (Mage::helper('core')->isModuleOutputEnabled('Shareino_Sync')) {
+            echo json_encode(['status' => true], true);
+        } else {
+            echo json_encode(['status' => false], true);
+        }
+        //$this->loadLayout();
+        //$this->renderLayout();
     }
 
 }
